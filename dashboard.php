cb8f82936191f3f87e72d6aa4bb4d2520e8288f2
Shafat Alam <Shafat.mahtab@gmail.com>
2024-07-13 15:01:32 +0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -20,7 +20,7 @@ $res_id = $result['id'];
 
 <head>
     <meta charset="UTF-8">
-    <title>Welcome, <?php echo $_SESSION['username']; ?>!</title>
+    <title>Welcome!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width" />
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'>
